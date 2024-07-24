0bcb447f156d5e6f525e8a191e65dd90a1208b7b
storzmichael <micsto24@gmail.com>
2024-07-24 08:46:25 +0200

    added new colors

---
+++ lib/src/config/config_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const List<Color> lightTheme = [
+  Color.fromARGB(255, 255, 255, 255),
+  Color.fromARGB(255, 153, 153, 153),
+];
+
+const List<Color> darkTheme = [
+  Color.fromARGB(255, 158, 158, 158),
+  Color.fromARGB(255, 56, 56, 56),
+];
+
+const Color orangeColor = Color.fromARGB(255, 246, 118, 0);
