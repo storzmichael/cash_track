64a05ba1ec8b7a0e2d6e59d508dcd8e513e8d68b
storzmichael <micsto24@gmail.com>
2024-07-24 08:48:34 +0200

    added new item list

---
+++ lib/src/features/events/data/event_button_data.dart
@@ -0,0 +1,22 @@
+import 'package:cash_track/src/features/events/domain/event_button_item.dart';
+import 'package:flutter/material.dart';
+
+List<EventButtonItem> eventButtonData = [
+  // collection of data
+  const EventButtonItem(
+    eventButtonTitle: "Neues Event",
+    eventButtonIcon: Icons.add,
+  ),
+  const EventButtonItem(
+    eventButtonTitle: "Neues Event",
+    eventButtonIcon: Icons.add,
+  ),
+  const EventButtonItem(
+    eventButtonTitle: "Neues Event",
+    eventButtonIcon: Icons.add,
+  ),
+  const EventButtonItem(
+    eventButtonTitle: "Neues Event",
+    eventButtonIcon: Icons.add,
+  ),
+];
