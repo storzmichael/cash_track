1075c85b3b5729e3fa984beabc4c277a2def6aa1
storzmichael <micsto24@gmail.com>
2024-07-24 08:48:19 +0200

    added new item

---
+++ lib/src/features/events/domain/event_button_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EventButtonItem {
+  final String eventButtonTitle;
+  final IconData eventButtonIcon;
+
+  const EventButtonItem({
+    required this.eventButtonTitle,
+    required this.eventButtonIcon,
+  });
+}
