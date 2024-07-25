9fa9c2b2932846410d74789ef265e9a5d6139b2a
storzmichael <micsto24@gmail.com>
2024-07-25 21:27:19 +0200

    new list added

---
+++ lib/src/features/events/data/event_data.dart
@@ -0,0 +1,53 @@
+import 'package:cash_track/src/features/events/domain/event_button_item.dart';
+
+List<EventItem> eventDatas = [
+  // collection of data
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+  const EventItem(
+    eventTitle: 'Eventname',
+    eventDate: 'Datum',
+  ),
+];
