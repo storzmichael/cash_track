45fd8626e8273556929795994f2b9ca4fbf64baf
storzmichael <micsto24@gmail.com>
2024-07-25 09:31:58 +0200

    changed folder

---
+++ lib/src/features/events/presentation/layout_widgets/event_grid.dart
@@ -0,0 +1,31 @@
+import 'package:cash_track/src/features/events/data/event_button_data.dart';
+import 'package:cash_track/src/features/events/presentation/single_widgets/event_button.dart';
+import 'package:flutter/material.dart';
+
+class EventGrid extends StatelessWidget {
+  const EventGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 312,
+      child: GridView.builder(
+        shrinkWrap: true,
+        itemCount: eventButtonData.length,
+        padding: EdgeInsets.zero,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 32,
+          crossAxisSpacing: 32,
+          mainAxisExtent: 140,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return EventButton(
+            eventButtonItem: eventButtonData[index],
+            onPressed: () {},
+          );
+        },
+      ),
+    );
+  }
+}
