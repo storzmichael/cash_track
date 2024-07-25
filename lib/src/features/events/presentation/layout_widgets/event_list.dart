4e138a9cb8b19f054b50ad0fe575ba8f95bf9ff4
storzmichael <micsto24@gmail.com>
2024-07-25 21:32:36 +0200

    create new listView

---
+++ lib/src/features/events/presentation/layout_widgets/event_list.dart
@@ -0,0 +1,27 @@
+import 'package:cash_track/src/config/button_varibals.dart';
+import 'package:cash_track/src/features/events/data/event_data.dart';
+
+import 'package:cash_track/src/features/events/presentation/single_widgets/event_txt_field.dart';
+import 'package:flutter/material.dart';
+
+class EventList extends StatelessWidget {
+  const EventList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(borderRadiusSmallButton)),
+        child: ListView.builder(
+          itemCount: eventDatas.length,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) {
+            return EventTxtField(
+              eventItem: eventDatas[index],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
