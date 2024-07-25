526507671e523436fcc234b691d0dff1b0a66e81
storzmichael <micsto24@gmail.com>
2024-07-25 21:24:09 +0200

    make AppHome const

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
-      home: AppHome(),
+      home: const AppHome(),
     );
   }
 }
