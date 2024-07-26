0d6edbdc0aef9d31fd29526736f8cdf3c932d96a
storzmichael <micsto24@gmail.com>
2024-07-26 02:06:10 +0200

    added new variable 'eventButtonOnPressed'

--- lib/src/features/events/domain/event_button_item.dart
+++ lib/src/features/events/domain/event_button_item.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class EventButtonItem {
   final String eventButtonTitle;
   final IconData eventButtonIcon;
+  final Function(BuildContext) eventButtonOnPressed;
 
   const EventButtonItem({
     required this.eventButtonTitle,
     required this.eventButtonIcon,
+    required this.eventButtonOnPressed,
   });
 }
