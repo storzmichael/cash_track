e1348e9f053f3adad5a0d1c1362c218cab36f532
storzmichael <micsto24@gmail.com>
2024-07-26 15:04:27 +0200

    create new screen

---
+++ lib/src/features/events/presentation/edit_event_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditEventScreen extends StatelessWidget {
+  const EditEventScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
