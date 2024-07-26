18afc9da9905e198f4fb185c9d428fdd2d5883ed
storzmichael <micsto24@gmail.com>
2024-07-26 02:09:35 +0200

    create new textfield block

---
+++ lib/src/features/events/presentation/layout_widgets/txt_fields_event.dart
@@ -0,0 +1,45 @@
+import 'package:cash_track/src/features/events/data/event_textfield_data.dart';
+import 'package:cash_track/src/features/general_widgets/presentation/big_button.dart';
+import 'package:cash_track/src/features/general_widgets/presentation/custom_txt_field.dart';
+import 'package:flutter/material.dart';
+
+class TextFieldEvent extends StatelessWidget {
+  const TextFieldEvent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          CustomTextField(
+            eventTextfieldItem: eventTextfieldDatas[0],
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          CustomTextField(
+            eventTextfieldItem: eventTextfieldDatas[1],
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            eventTextfieldItem: eventTextfieldDatas[2],
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            eventTextfieldItem: eventTextfieldDatas[3],
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          const BigButton(buttonName: 'Generiere neuen Code'),
+          const Expanded(child: SizedBox()),
+          const BigButton(buttonName: 'Start Event'),
+        ],
+      ),
+    );
+  }
+}
