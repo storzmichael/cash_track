02f365ceabe2585ea8100735a14b6d9fd236854b
storzmichael <micsto24@gmail.com>
2024-07-26 15:03:59 +0200

    added new button function

--- lib/src/features/events/application/button_funktions.dart
+++ lib/src/features/events/application/button_funktions.dart
@@ -1,9 +1,17 @@
 import 'package:cash_track/src/features/events/presentation/create_event_screen.dart';
+import 'package:cash_track/src/features/events/presentation/edit_event_screen.dart';
 import 'package:flutter/material.dart';
 
 void navigateToCreateEventScreen(BuildContext context) {
   Navigator.push(
     context,
     MaterialPageRoute(builder: (context) => const CreateEventScreen()),
+  );
+}
+
+void navigateToEditEventScreen(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const EditEventScreen()),
   );
 }
