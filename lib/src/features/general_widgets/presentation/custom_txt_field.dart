a3992702f59b670cd742de0b652635b74cf633a9
storzmichael <micsto24@gmail.com>
2024-09-02 17:41:15 +0200

    changed color

--- lib/src/features/general_widgets/presentation/custom_txt_field.dart
+++ lib/src/features/general_widgets/presentation/custom_txt_field.dart
@@ -3,22 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:cash_track/src/config/button_varibals.dart';
 import 'package:cash_track/src/config/config.dart';
 import 'package:cash_track/src/config/config_colors.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
-  final EventTextfieldItem eventTextfieldItem;
   final TextEditingController? controller;
+  final EventTextfieldItem eventTextfieldItem;
+  final TextInputType keyboardType;
+  final TextInputFormatter? inputFormatter;
 
   const CustomTextField({
     super.key,
-    required this.eventTextfieldItem,
     this.controller,
+    required this.eventTextfieldItem,
+    this.keyboardType = TextInputType.text,
+    this.inputFormatter,
+    required Null Function(dynamic value) onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: textFieldColor,
+        color: greyColor,
         borderRadius: BorderRadius.circular(borderRadiusSmallButton),
       ),
       height: 40,
