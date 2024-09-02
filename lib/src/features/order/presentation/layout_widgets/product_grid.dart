641024e2d1830ae02969a73f1bc152788887035d
storzmichael <micsto24@gmail.com>
2024-09-02 17:43:17 +0200

    changed argumments

--- lib/src/features/order/presentation/layout_widgets/product_grid.dart
+++ lib/src/features/order/presentation/layout_widgets/product_grid.dart
@@ -1,15 +1,18 @@
 import 'package:cash_track/src/features/order/data/products_list.dart';
+import 'package:cash_track/src/features/order/domain/product_item.dart';
 import 'package:cash_track/src/features/order/presentation/single_widgets/button_product_custom.dart';
 import 'package:flutter/material.dart';
 
 class ProductGrid extends StatelessWidget {
-  const ProductGrid({super.key});
+  final List<ProductItem> productsBttnList;
+
+  const ProductGrid({super.key, required this.productsBttnList});
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       shrinkWrap: true,
-      itemCount: productsData.length,
+      itemCount: productsBttnList.length,
       padding: EdgeInsets.zero,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 4,
@@ -19,7 +22,7 @@ class ProductGrid extends StatelessWidget {
       ),
       itemBuilder: (BuildContext context, int index) {
         return ButtonProductCustom(
-          products: productsData[index],
+          products: productsBttnList[index],
           onPressed: () {},
         );
       },
