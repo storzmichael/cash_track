f359308e329c0e44c414be66063b5baec929b13b
storzmichael <micsto24@gmail.com>
2024-09-03 15:47:45 +0200

    adjusted to the functions

--- lib/src/features/order/presentation/layout_widgets/product_grid.dart
+++ lib/src/features/order/presentation/layout_widgets/product_grid.dart
@@ -1,29 +1,65 @@
-import 'package:cash_track/src/features/order/data/products_list.dart';
-import 'package:cash_track/src/features/order/domain/product_item.dart';
+// import 'package:cash_track/src/features/order/domain/product_item.dart';
+// import 'package:cash_track/src/features/order/presentation/single_widgets/button_product_custom.dart';
+// import 'package:flutter/material.dart';
+
+// class ProductGrid extends StatelessWidget {
+//   final List<ProductItem> productItemsList;
+
+//   const ProductGrid({super.key, required this.productItemsList});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GridView.builder(
+//       shrinkWrap: true,
+//       itemCount: productItemsList.length,
+//       padding: EdgeInsets.zero,
+//       physics: const NeverScrollableScrollPhysics(),
+//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//         crossAxisCount: 4,
+//         mainAxisSpacing: 20,
+//         crossAxisSpacing: 12,
+//         mainAxisExtent: 60,
+//       ),
+//       itemBuilder: (BuildContext context, int index) {
+//         ProductItem product = productItemsList[index];
+//         return ButtonProductCustom(
+//           products: product,
+//           onPressed: () {},
+//         );
+//       },
+//     );
+//   }
+// }
+
 import 'package:cash_track/src/features/order/presentation/single_widgets/button_product_custom.dart';
 import 'package:flutter/material.dart';
+import 'package:cash_track/src/features/order/domain/product_item.dart';
 
 class ProductGrid extends StatelessWidget {
-  final List<ProductItem> productsBttnList;
+  final List<ProductItem> productItemsList;
 
-  const ProductGrid({super.key, required this.productsBttnList});
+  const ProductGrid({
+    super.key,
+    required this.productItemsList,
+  });
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      shrinkWrap: true,
-      itemCount: productsBttnList.length,
-      padding: EdgeInsets.zero,
+      physics: const NeverScrollableScrollPhysics(),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 4,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 12,
-        mainAxisExtent: 60,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
       ),
-      itemBuilder: (BuildContext context, int index) {
+      itemCount: productItemsList.length,
+      itemBuilder: (context, index) {
+        final item = productItemsList[index];
         return ButtonProductCustom(
-          products: productsBttnList[index],
-          onPressed: () {},
+          productItem: item,
+          onPressed: () {
+            print('${item.productPrice}');
+          },
         );
       },
     );
