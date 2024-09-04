9be3630aa7b48dedf7f064a1328397a2c3a1ff5c
storzmichael <micsto24@gmail.com>
2024-09-04 16:09:28 +0200

    added new functions

--- lib/src/features/order/presentation/order_screen.dart
+++ lib/src/features/order/presentation/order_screen.dart
@@ -1,14 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:cash_track/src/config/button_varibals.dart';
 import 'package:cash_track/src/config/config_colors.dart';
 import 'package:cash_track/src/core/presentation/theme_container.dart';
 import 'package:cash_track/src/features/general_widgets/presentation/big_button.dart';
-import 'package:cash_track/src/features/general_widgets/presentation/outlined_big_button.dart';
 import 'package:cash_track/src/features/order/data/category_data_map.dart';
-
+import 'package:cash_track/src/features/order/data/table_list.dart';
 import 'package:cash_track/src/features/order/presentation/layout_widgets/category_row.dart';
 import 'package:cash_track/src/features/order/presentation/layout_widgets/monitor_view.dart';
-
-import 'package:flutter/material.dart';
 
 class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
@@ -18,20 +16,113 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  final bool _isSelect = true;
-  final int _deskNumber = 1; //test
-
+  bool isTableSelect = false; // Status der Auswahl, initial auf false gesetzt
+  String deskNumber = ''; // Aktuelle Tischnummer
   final bool _isContainerEmpty = false;
+
+  void _addNewButton(String buttonName) {
+    if (buttonName.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text('Bitte geben Sie etwas ein'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+
+    if (tables.contains(buttonName)) {
+      // Zeige Fehlermeldung an, wenn der Name bereits existiert
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Tisch mit dem Namen "$buttonName" existiert bereits!'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+
+    setState(() {
+      tables.add(buttonName);
+      tables.sort(); // Sortiere die Liste alphabetisch
+    });
+  }
+
+  void showAddButtonDialog() {
+    final TextEditingController buttonTextController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Neuen Button hinzufügen'),
+          content: TextField(
+            controller: buttonTextController,
+            decoration: const InputDecoration(
+              hintText: 'Button Name eingeben',
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Abbrechen'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Hinzufügen'),
+              onPressed: () {
+                String buttonName = buttonTextController.text.trim();
+                _addNewButton(buttonName);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDeleteConfirmDialog(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Button löschen'),
+          content: const Text('Möchten Sie diesen Button wirklich löschen?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Abbrechen'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Löschen'),
+              onPressed: () {
+                setState(() {
+                  tables.removeAt(index);
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: _isSelect ? Text('Tischnummer: $_deskNumber') : const Text(''),
-        actions: const [
-          Icon(Icons.settings),
-          SizedBox(
-            width: 16,
+        title: isTableSelect ? Text('Tischnummer: $deskNumber') : const Text('Tischnummer wählen'),
+        actions: [
+          IconButton(
+            onPressed: showAddButtonDialog,
+            icon: const Icon(Icons.add),
           ),
+          const SizedBox(width: 16),
         ],
       ),
       body: Stack(
@@ -43,37 +134,56 @@ class _OrderScreenState extends State<OrderScreen> {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 const MonitorView(),
-                const SizedBox(
-                  height: 24,
-                ),
-                SizedBox(
-                  height: 40,
-                  width: 200,
-                  child: OutlinedBigButton(
-                    buttonName: 'Tischnummer wählen',
-                    onPressed: () {},
-                  ),
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                Expanded(
-                  child: CategoryRow(
-                    category: categoryData,
-                  ),
-                ),
+                const SizedBox(height: 24),
+                isTableSelect
+                    ? Expanded(
+                        child: CategoryRow(
+                          category: categoryData,
+                        ),
+                      )
+                    : Expanded(
+                        child: Scrollbar(
+                          child: ListView.builder(
+                            itemCount: tables.length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 4.0),
+                                child: GestureDetector(
+                                  onLongPress: () => _showDeleteConfirmDialog(index),
+                                  child: SizedBox(
+                                    height: 40,
+                                    child: BigButton(
+                                      backgroundColor: Colors.white,
+                                      onPressed: () {
+                                        setState(() {
+                                          deskNumber = tables[index];
+                                          isTableSelect = true; // Setze isTableSelect auf true
+                                        });
+                                      },
+                                      buttonName: tables[index],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
                 SizedBox(
                   height: bigBttnHeight,
                   child: BigButton(
-                    buttonName: 'Bestellen',
-                    backgroundColor: _isContainerEmpty ? Colors.grey.shade300 : orangeColor,
-                    textColor: _isContainerEmpty ? Colors.grey.shade500 : Colors.black,
-                    onPressed: _isContainerEmpty ? null : () {},
-                  ),
+                      buttonName: 'Bestellen',
+                      backgroundColor: isTableSelect ? orangeColor : Colors.grey.shade300,
+                      textColor: isTableSelect ? Colors.black : Colors.grey.shade500,
+                      onPressed: isTableSelect
+                          ? () {
+                              setState(() {
+                                isTableSelect = false; // Setze isTableSelect zurück
+                              });
+                            }
+                          : null),
                 ),
-                const SizedBox(
-                  height: 50,
-                ),
+                const SizedBox(height: 50),
               ],
             ),
           ),
