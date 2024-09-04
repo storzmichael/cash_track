23ed8eed49afd2b0f637cdf5261fa5e63b86e8fd
storzmichael <micsto24@gmail.com>
2024-09-04 16:06:25 +0200

    deleted imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:cash_track/src/config/theme_data.dart';
 import 'package:cash_track/src/core/presentation/app_home.dart';
-import 'package:cash_track/src/features/cashout/presentation/cashout_screen.dart';
+
 import 'package:cash_track/src/features/order/presentation/order_screen.dart';
-import 'package:cash_track/src/features/registration-login/presentation/login_screen.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
