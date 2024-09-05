c47238d5c7aef98e9ad645f078f1a1cf22462fb9
storzmichael <micsto24@gmail.com>
2024-09-05 15:26:40 +0200

    changed color

--- lib/src/config/theme_data.dart
+++ lib/src/config/theme_data.dart
@@ -1,19 +1,20 @@
 import 'package:cash_track/src/config/config_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData lightTheme = ThemeData(
-  hintColor: Colors.orange,
-  fontFamily: 'SFProRounded',
-  textTheme: const TextTheme(
-    bodySmall: TextStyle(fontSize: 12),
-    bodyMedium: TextStyle(fontSize: 15),
-
-    headlineSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-    headlineLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-
-    labelSmall: TextStyle(fontSize: 14, fontWeight: FontWeight.bold), // for small buttons
-    labelMedium: TextStyle(fontSize: 16), // for symbols
-    labelLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), // for buttons
+  hintColor: primeryColor,
+  textTheme: GoogleFonts.poppinsTextTheme(
+    // Hier wird die Poppins-Schriftart geladen
+    const TextTheme(
+      bodySmall: TextStyle(fontSize: 12),
+      bodyMedium: TextStyle(fontSize: 15),
+      headlineSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+      headlineLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      labelSmall: TextStyle(fontSize: 14, fontWeight: FontWeight.bold), // for small buttons
+      labelMedium: TextStyle(fontSize: 16), // for symbols
+      labelLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), // for buttons
+    ),
   ),
   appBarTheme: const AppBarTheme(
     backgroundColor: Color.fromARGB(255, 255, 255, 255),
@@ -25,7 +26,7 @@ ThemeData lightTheme = ThemeData(
   ),
   textButtonTheme: TextButtonThemeData(
     style: TextButton.styleFrom(
-      foregroundColor: orangeColor,
+      foregroundColor: primeryColor,
     ),
   ),
 );
