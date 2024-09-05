6fdc63091176979a7b60b2aa114c4fcd71e132c0
storzmichael <micsto24@gmail.com>
2024-09-05 15:27:22 +0200

    deleted

--- lib/src/core/data/lang/app_text.dart
+++
@@ -1,7 +0,0 @@
-import 'package:cash_track/src/core/data/lang/english.dart';
-import 'package:cash_track/src/core/data/lang/german.dart';
-
-Map<String, List<String>> textFiles = {
-  'Deutsch': german,
-  'English': english,
-};
