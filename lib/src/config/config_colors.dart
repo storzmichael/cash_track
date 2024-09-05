4f719637d15c1af6753fae9465fba72e4dcfed1f
storzmichael <micsto24@gmail.com>
2024-09-05 15:25:29 +0200

    changed colors

--- lib/src/config/config_colors.dart
+++ lib/src/config/config_colors.dart
@@ -10,9 +10,16 @@ const List<Color> darkTheme = [
   Color.fromARGB(255, 56, 56, 56),
 ];
 
-const Color orangeColor = Color.fromARGB(255, 246, 118, 0);
+const Color primeryColor = Color.fromARGB(255, 246, 118, 0);
+const Color primeryColorLow = Color.fromARGB(255, 234, 173, 115);
 const Color greenColor = Color.fromARGB(255, 28, 98, 33);
-const Color greyColor = Color.fromARGB(255, 198, 198, 198);
+
 const Color blackColor = Colors.black;
 const Color alertColor = Colors.red;
 const Color whiteColor = Colors.white;
+const Color monitorColor = textFieldColor;
+const Color textFieldColor = Color.fromARGB(255, 198, 198, 198);
+const Color greyColor = Colors.grey;
+
+Color disabledTextColor = Colors.grey.shade500;
+Color disabledBttnColor = Colors.grey.shade300;
