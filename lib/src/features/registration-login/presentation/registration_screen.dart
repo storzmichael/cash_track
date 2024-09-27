bba42343a4780c5e762a865c12140756c3931b60
storzmichael <micsto24@gmail.com>
2024-09-27 14:37:09 +0200

    added firebase auth

--- lib/src/features/registration-login/presentation/registration_screen.dart
+++ lib/src/features/registration-login/presentation/registration_screen.dart
@@ -1,103 +1,158 @@
 import 'package:cash_track/src/config/config.dart';
+import 'package:cash_track/src/config/config_colors.dart';
+import 'package:cash_track/src/core/presentation/app_home.dart';
 import 'package:cash_track/src/data/lang/app_text.dart';
-
 import 'package:cash_track/src/core/presentation/theme_container.dart';
 import 'package:cash_track/src/features/general_widgets/presentation/big_button.dart';
+import 'package:cash_track/src/features/registration-login/presentation/shimmer_logo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:shimmer/shimmer.dart';
 import 'login_text_field.dart';
 
-class RegistrastionScreen extends StatefulWidget {
-  const RegistrastionScreen({super.key});
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
 
   @override
-  State<RegistrastionScreen> createState() => _RegistrationScreenState();
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
 }
 
-class _RegistrationScreenState extends State<RegistrastionScreen> {
-  final TextEditingController userController = TextEditingController(); // Controller für den Benutzernamen
-  final TextEditingController emailController = TextEditingController(); // Controller für die E-Mail-Adresse
-  final TextEditingController passwordController = TextEditingController(); // Controller für das Passwort
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  String? _errorMassage = '';
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _createUserWithEmailandPassword() async {
+    if (_emailController.text.isEmpty || _passwordController.text.isEmpty) {
+      setState(() {
+        _errorMassage = textFiles[language]![50];
+      });
+      _errorAlert();
+      return;
+    }
+
+    try {
+      // Überprüfen, ob ein Konto mit der E-Mail-Adresse existiert
+      final signInMethods = await FirebaseAuth.instance.fetchSignInMethodsForEmail(_emailController.text);
+
+      if (signInMethods.isNotEmpty) {
+        // Konto existiert bereits
+        setState(() {
+          _errorMassage = textFiles[language]![51];
+        });
+        _errorAlert();
+        return;
+      }
+
+      // Konto erstellen, wenn es nicht existiert
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      // Nur bei erfolgreicher Registrierung zum nächsten Bildschirm wechseln
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const AppHome()),
+        (route) => false, // Entfernt alle vorherigen Routen
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMassage = e.message ?? textFiles[language]![52];
+      });
+      _errorAlert();
+    } catch (e) {
+      setState(() {
+        _errorMassage = '${textFiles[language]![52]}: $e';
+      });
+      _errorAlert();
+    }
+  }
+
+  void _errorAlert() {
+    if (_errorMassage != null && _errorMassage!.isNotEmpty) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(textFiles[language]![54]),
+            content: Text(
+              _errorMassage!,
+              style: const TextStyle(fontSize: 13, color: alertColor),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text(textFiles[language]![55]),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  Widget _logo() {
+    return const ShimmerLogo();
+  }
+
+  Widget _emailTextField() {
+    return LoginTextField(
+      labelName: textFiles[language]![22],
+      controller: _emailController,
+      prefixIcon: const Icon(Icons.person_2_outlined),
+    );
+  }
+
+  Widget _passwordTextField() {
+    return LoginTextField(
+      labelName: textFiles[language]![23],
+      isPassword: true,
+      controller: _passwordController,
+      prefixIcon: const Icon(Icons.lock_outline_rounded),
+    );
+  }
+
+  Widget _registerButton() {
+    return BigButton(
+      onPressed: _createUserWithEmailandPassword,
+      buttonName: textFiles[language]![35],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-        title: Text(
-          textFiles[language]![20], // Titel der AppBar
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
+        title: Text(textFiles[language]![48]),
       ),
       body: Stack(
         children: [
-          const ThemeContainer(), // Hintergrundthema
+          const ThemeContainer(),
           Padding(
             padding: const EdgeInsets.fromLTRB(bottomPadding, sitesPadding, bottomPadding, sitesPadding),
             child: Column(
               children: [
-                Center(
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: logoheight,
-                        width: logowidth,
-                        child: Image(
-                          image: AssetImage(appImagePath), // Logo der App
-                          fit: BoxFit.fitHeight,
-                        ),
-                      ),
-                      SizedBox(
-                        child: Shimmer.fromColors(
-                          period: const Duration(milliseconds: 3000),
-                          loop: 3,
-                          baseColor: Colors.black,
-                          highlightColor: Colors.grey,
-                          child: const Text(
-                            appName, // Name der App mit Schimmer-Effekt
-                            style: TextStyle(fontFamily: 'RubikOne', fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                      Text(
-                        textFiles[language]![21], // Zusätzlicher Text unter dem Logo
-                      ),
-                    ],
-                  ),
+                const SizedBox(
+                  height: 100 - 56,
                 ),
+                _logo(),
+                const SizedBox(
+                  height: 124,
+                ),
+                _emailTextField(),
+                const SizedBox(
+                  height: 8,
+                ),
+                _passwordTextField(),
                 const SizedBox(
                   height: 32,
                 ),
-                // BENUTZERNAME TEXTFELD
-                LoginTextField(
-                  labelName: textFiles[language]![33], // Bezeichnung des Benutzernamenfeldes
-
-                  controller: userController,
-                ),
+                _registerButton(),
                 const SizedBox(
-                  height: 12,
-                ),
-                // E-MAIL TEXTFELD
-                LoginTextField(
-                  labelName: textFiles[language]![22], // Bezeichnung des E-Mail-Feldes
-                  controller: emailController,
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                // PASSWORT TEXTFELD
-                LoginTextField(
-                  labelName: textFiles[language]![23], // Bezeichnung des Passwortfeldes
-                  isPassword: true,
-                  controller: passwordController,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                BigButton(
-                  onPressed: () {},
-                  buttonName: textFiles[language]![35], // Text des Buttons
+                  height: 8,
                 ),
               ],
             ),
