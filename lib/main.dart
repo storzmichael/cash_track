2be3b159fa727dab931955e8b689ba30220f5cf2
storzmichael <micsto24@gmail.com>
2024-09-27 14:29:05 +0200

    added const

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
