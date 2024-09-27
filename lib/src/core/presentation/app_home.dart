68d03ec1ecb2f26551a9a4d5d9ed568309e25c12
storzmichael <micsto24@gmail.com>
2024-09-27 14:29:44 +0200

    added comments

--- lib/src/core/presentation/app_home.dart
+++ lib/src/core/presentation/app_home.dart
@@ -1,9 +1,9 @@
+import 'package:cash_track/src/config/config_colors.dart';
 import 'package:cash_track/src/data/nav_item_data.dart';
 import 'package:cash_track/src/features/events/presentation/screens/event_screen.dart';
 import 'package:cash_track/src/features/events/presentation/screens/favorites_screen.dart';
-import 'package:cash_track/src/features/profil/presentation/profil_screen.dart';
+import 'package:cash_track/src/features/settings/presentation/setting_screen.dart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class AppHome extends StatefulWidget {
   const AppHome({super.key});
@@ -24,61 +24,55 @@ class _AppHomeState extends State<AppHome> {
   final List<Widget> screens = [
     const EventScreen(),
     const FavoritesScreen(),
-    const ProfilScreen(),
+    const SettingScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle.dark, // Changes status bar text color to dark
-        title: Text(
-          navigationItemData[currentIndex].label, // Sets the title according to the current screen
-          style: const TextStyle(fontWeight: FontWeight.bold),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: blackColor, // Set the color of the border
+              width: 0.07, // Set the width of the border
+            ),
+          ),
         ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            // TODO: Navigation to settings screen
-            onPressed: () {},
-          ),
-        ],
+        child: BottomNavigationBar(
+          currentIndex: currentIndex, // Highlights the currently selected item
+          onTap: (index) {
+            setState(() {
+              currentIndex = index; // Updates the screen based on selected item
+            });
+          },
+          showSelectedLabels: true, // Shows label for the selected item
+          showUnselectedLabels: false, // Hides labels for unselected items
+          items: [
+            // Navigate to Event screen
+            BottomNavigationBarItem(
+              activeIcon: Icon(navigationItemData[0].activeIcon), // Active icon for event screen
+              icon: Icon(navigationItemData[0].icon), // Default icon for event screen
+              label: navigationItemData[0].label, // Label for event screen
+            ),
+
+            // Navigate to Favorites screen
+            BottomNavigationBarItem(
+              activeIcon: Icon(navigationItemData[1].activeIcon), // Active icon for favorites screen
+              icon: Icon(navigationItemData[1].icon), // Default icon for favorites screen
+              label: navigationItemData[1].label, // Label for favorites screen
+            ),
+
+            // Navigate to Profile screen
+            BottomNavigationBarItem(
+              activeIcon: Icon(navigationItemData[2].activeIcon), // Active icon for profile screen
+              icon: Icon(navigationItemData[2].icon), // Default icon for profile screen
+              label: navigationItemData[2].label, // Label for profile screen
+            ),
+          ],
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex, // Highlights the currently selected item
-        onTap: (index) {
-          setState(() {
-            currentIndex = index; // Updates the screen based on selected item
-          });
-        },
-        showSelectedLabels: true, // Shows label for the selected item
-        showUnselectedLabels: false, // Hides labels for unselected items
-        items: [
-          // Navigate to Event screen
-          BottomNavigationBarItem(
-            activeIcon: Icon(navigationItemData[0].activeIcon), // Active icon for event screen
-            icon: Icon(navigationItemData[0].icon), // Default icon for event screen
-            label: navigationItemData[0].label, // Label for event screen
-          ),
-
-          // Navigate to Favorites screen
-          BottomNavigationBarItem(
-            activeIcon: Icon(navigationItemData[1].activeIcon), // Active icon for favorites screen
-            icon: Icon(navigationItemData[1].icon), // Default icon for favorites screen
-            label: navigationItemData[1].label, // Label for favorites screen
-          ),
-
-          // Navigate to Profile screen
-          BottomNavigationBarItem(
-            activeIcon: Icon(navigationItemData[2].activeIcon), // Active icon for profile screen
-            icon: Icon(navigationItemData[2].icon), // Default icon for profile screen
-            label: navigationItemData[2].label, // Label for profile screen
-          ),
-        ],
-      ),
-
-      // Shows the currently selected screen from the list
-      body: screens[currentIndex],
+      body: screens[currentIndex], // Shows the currently selected screen from the list
     );
   }
 }
