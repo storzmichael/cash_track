130e2aaf42a0c268d518ec9a7b05bf1da67030c2
storzmichael <micsto24@gmail.com>
2024-09-27 14:34:39 +0200

    changed widgets

--- lib/src/features/events/presentation/screens/favorites_screen.dart
+++ lib/src/features/events/presentation/screens/favorites_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cash_track/src/config/config.dart';
 import 'package:cash_track/src/core/presentation/theme_container.dart';
+import 'package:cash_track/src/data/lang/app_text.dart';
 import 'package:cash_track/src/features/events/presentation/layout_widgets/event_list.dart';
 
 import 'package:flutter/material.dart';
@@ -7,32 +8,44 @@ import 'package:flutter/material.dart';
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
+  Widget _logo() {
+    return const Center(
+      child: SizedBox(
+        height: 150,
+        child: Image(
+          image: AssetImage(appImagePath),
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Stack(
-      children: [
-        ThemeContainer(),
-        Padding(
-          padding: EdgeInsets.all(sitesPadding),
-          child: Column(
-            children: [
-              Center(
-                child: SizedBox(
-                  height: 150,
-                  child: Image(
-                    image: AssetImage(appImagePath),
-                    fit: BoxFit.fitHeight,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              EventList(),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          textFiles[language]![1],
+          style: Theme.of(context).textTheme.labelLarge,
         ),
-      ],
+      ),
+      body: Stack(
+        children: [
+          const ThemeContainer(),
+          Padding(
+            padding: const EdgeInsets.all(sitesPadding),
+            child: Column(
+              children: [
+                _logo(),
+                const SizedBox(
+                  height: 32,
+                ),
+                const EventList(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
