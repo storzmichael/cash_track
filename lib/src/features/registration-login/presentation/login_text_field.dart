b404de515426d20c1420af9786560238d3ed85a2
storzmichael <micsto24@gmail.com>
2024-09-27 14:36:48 +0200

    refactory

--- lib/src/features/registration-login/presentation/login_text_field.dart
+++ lib/src/features/registration-login/presentation/login_text_field.dart
@@ -2,89 +2,81 @@ import 'package:cash_track/src/config/config_colors.dart';
 import 'package:flutter/material.dart';
 
 class LoginTextField extends StatefulWidget {
-  final String labelName; // Bezeichnung des Textfelds
-  final bool isPassword; // Gibt an, ob es sich um ein Passwortfeld handelt
-  final TextEditingController controller; // Controller für das Textfeld
-  final Color borderColor; // Farbe des Rahmens des Textfelds
-  final Color focusedBorderColor; // Farbe des Rahmens, wenn das Textfeld den Fokus hat
-  final Color errorBorderColor; // Farbe des Rahmens bei einem Fehler
-  final Color labelTextColor; // Farbe des Bezeichnungs-Textes
+  final String labelName;
+  final bool isPassword;
+  final TextEditingController controller;
+  final Color labelTextColor;
   final double borderRadius; // Radius für die Ecken
-  final Color backgroundColor; // Hintergrundfarbe des Textfelds
-  final double height; // Höhe des Textfeldes
-  final Widget? prefixIcon; // Optionales Icon vor dem Hint-Text
+  final Color backgroundColor;
+  final double height;
+  final Widget? prefixIcon;
 
   const LoginTextField({
     super.key,
     required this.labelName,
     this.isPassword = false,
     required this.controller,
-    this.borderColor = Colors.red, // Standard-Rahmenfarbe
-    this.focusedBorderColor = textFieldColor,
-    this.errorBorderColor = alertColor,
     this.labelTextColor = whiteColor,
     this.borderRadius = 12,
-    this.backgroundColor = textFieldColor, // Standard-Hintergrundfarbe
-    this.height = 40, // Standard-Höhe des Textfeldes
-    this.prefixIcon, // Optionales Icon
+    this.backgroundColor = textFieldColor,
+    this.height = 40,
+    this.prefixIcon,
   });
 
   @override
-  // ignore: library_private_types_in_public_api
   _LoginTextFieldState createState() => _LoginTextFieldState();
 }
 
 class _LoginTextFieldState extends State<LoginTextField> {
-  bool _obscureText = true; // Steuert, ob der Text des Passwortfeldes verborgen ist
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: widget.height,
-      child: TextField(
-        controller: widget.controller, // Controller für das Textfeld
-        decoration: InputDecoration(
-          filled: true, // Hintergrundfarbe aktivieren
-          fillColor: widget.backgroundColor, // Hintergrundfarbe des Textfeldes
-          border: OutlineInputBorder(
-            borderSide: BorderSide(color: widget.borderColor), // Rahmenfarbe des Textfelds
-            borderRadius: BorderRadius.circular(widget.borderRadius), // Abgerundete Ecken
+    return Container(
+      decoration: BoxDecoration(
+        color: widget.backgroundColor,
+        borderRadius: BorderRadius.circular(widget.borderRadius), // Radius für die Ecken
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(widget.borderRadius), // Radius für die Ecken
+        child: SizedBox(
+          height: widget.height,
+          child: TextField(
+            controller: widget.controller,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: widget.backgroundColor,
+              border: InputBorder.none, // Kein Rahmen
+              focusedBorder: InputBorder.none, // Kein Rahmen, wenn im Fokus
+              errorBorder: InputBorder.none, // Kein Rahmen bei Fehler
+              labelText: widget.labelName,
+              labelStyle: TextStyle(color: widget.labelTextColor),
+              floatingLabelStyle: TextStyle(color: widget.labelTextColor),
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              contentPadding: EdgeInsets.symmetric(vertical: (widget.height - 20) / 2, horizontal: 12),
+              prefixIcon: widget.prefixIcon != null
+                  ? IconTheme(
+                      data: IconThemeData(color: widget.labelTextColor),
+                      child: widget.prefixIcon!,
+                    )
+                  : null,
+              suffixIcon: widget.isPassword
+                  ? IconButton(
+                      icon: Icon(
+                        color: widget.labelTextColor,
+                        _obscureText ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    )
+                  : null,
+            ),
+            obscureText: widget.isPassword ? _obscureText : false,
           ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: widget.focusedBorderColor), // Rahmenfarbe bei Fokus
-            borderRadius: BorderRadius.circular(widget.borderRadius), // Abgerundete Ecken bei Fokus
-          ),
-          errorBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: widget.errorBorderColor), // Rahmenfarbe bei Fehler
-            borderRadius: BorderRadius.circular(widget.borderRadius), // Abgerundete Ecken bei Fehler
-          ),
-          labelText: widget.labelName, // Bezeichnung des Textfelds
-          labelStyle: TextStyle(color: widget.labelTextColor), // Stil der Bezeichnung
-          floatingLabelStyle: TextStyle(color: widget.focusedBorderColor), // Stil der schwebenden Bezeichnung
-          floatingLabelBehavior: FloatingLabelBehavior.never, // Label soll nicht in den oberen Bereich verschieben
-          contentPadding: EdgeInsets.symmetric(
-              vertical: (widget.height - 20) / 2, horizontal: 12), // Vertikale Zentrierung des Textes
-          prefixIcon: widget.prefixIcon != null
-              ? IconTheme(
-                  data: IconThemeData(color: widget.labelTextColor), // Setzt die Farbe des Icons
-                  child: widget.prefixIcon!,
-                )
-              : null,
-          suffixIcon: widget.isPassword
-              ? IconButton(
-                  icon: Icon(
-                    color: widget.labelTextColor,
-                    _obscureText ? Icons.visibility : Icons.visibility_off, // Sichtbarkeit des Passworts steuern
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscureText = !_obscureText; // Text anzeigen oder verbergen
-                    });
-                  },
-                )
-              : null,
         ),
-        obscureText: widget.isPassword ? _obscureText : false, // Steuert, ob der Text verborgen ist
       ),
     );
   }
