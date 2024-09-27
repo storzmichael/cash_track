497d9ab897c29cee8d79aaef1961457ae5046209
storzmichael <micsto24@gmail.com>
2024-09-27 14:36:21 +0200

    added firebase auth

--- lib/src/features/registration-login/presentation/login_screen.dart
+++ lib/src/features/registration-login/presentation/login_screen.dart
@@ -1,14 +1,12 @@
 import 'package:cash_track/src/config/config.dart';
-
+import 'package:cash_track/src/config/config_colors.dart';
+import 'package:cash_track/src/core/presentation/app_home.dart';
 import 'package:cash_track/src/data/lang/app_text.dart';
-
 import 'package:cash_track/src/core/presentation/theme_container.dart';
 import 'package:cash_track/src/features/general_widgets/presentation/big_button.dart';
-
-import 'package:cash_track/src/features/registration-login/presentation/registration_screen.dart';
 import 'package:cash_track/src/features/registration-login/presentation/shimmer_logo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'login_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -19,11 +17,71 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _emailController = TextEditingController(); // Controller für das E-Mail-Feld
-  final TextEditingController _passwordController = TextEditingController(); // Controller für das Passwortfeld
+  String? _errorMassage = '';
 
-  bool rememberMe = false; // Status für die "Angemeldet bleiben"-Checkbox
-  final String correctPassword = '123'; // Korrektes Passwort für die Anmeldung
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _signInWithEmailandPassword() async {
+    if (_emailController.text.isEmpty || _passwordController.text.isEmpty) {
+      setState(() {
+        _errorMassage = textFiles[language]![69];
+      });
+      _errorAlert();
+      return;
+    }
+
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      if (userCredential.user != null) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const AppHome()),
+          (route) => false, // Entfernt alle vorherigen Routen
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMassage = e.message;
+      });
+      _errorAlert();
+    } catch (e) {
+      setState(() {
+        _errorMassage = '${textFiles[language]![70]}: $e';
+      });
+      _errorAlert();
+    }
+  }
+
+  void _errorAlert() {
+    if (_errorMassage != null && _errorMassage!.isNotEmpty) {
+      print('${textFiles[language]![53]}: $_errorMassage');
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(textFiles[language]![54]),
+            content: Text(
+              _errorMassage!,
+              style: const TextStyle(fontSize: 13, color: alertColor),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text(textFiles[language]![55]),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   Widget _logo() {
     return const ShimmerLogo();
@@ -31,63 +89,46 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Widget _emailTextField() {
     return LoginTextField(
-      labelName: textFiles[language]![22], // Label für das E-Mail-Feld
-      controller: _emailController, // Controller für das E-Mail-Feld
+      labelName: textFiles[language]![22],
+      controller: _emailController,
       prefixIcon: const Icon(Icons.person_2_outlined),
     );
   }
 
   Widget _passwordTextField() {
     return LoginTextField(
-      labelName: textFiles[language]![23], // Label für das E-Mail-Feld
+      labelName: textFiles[language]![23],
       isPassword: true,
-      controller: _passwordController, // Controller für das E-Mail-Feld
+      controller: _passwordController,
       prefixIcon: const Icon(Icons.lock_outline_rounded),
     );
   }
 
   Widget _loginButton() {
     return BigButton(
-      onPressed: () {},
-      buttonName: textFiles[language]![20], // Name des Anmeldebuttons aus der Sprachdatei
-    );
-  }
-
-  // ignore: unused_element
-  Widget _passwordForgottenButton() {
-    return TextButton(
-      onPressed: () {
-        // Aktion bei Passwort zurücksetzen
-      },
-      child: Text(
-        textFiles[language]![26], // Text für "Passwort zurücksetzen"
-        style: Theme.of(context).textTheme.bodySmall, // Textstil
-      ),
-    );
-  }
-
-  Widget _guestButton() {
-    return BigButton(
-      onPressed: () {
-        // Aktion bei Gast-Anmelde
-      },
-      buttonName: textFiles[language]![47], // Name des Buttons für den Gast-Anmelde aus der Sprachdatei
+      onPressed: _signInWithEmailandPassword,
+      buttonName: textFiles[language]![20],
     );
   }
 
   Widget _registerButton() {
     return TextButton(
       onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const RegistrastionScreen()), // Navigation zur Registrierungsseite
-        );
+        Navigator.pushNamed(context, "/registration");
       },
       child: Text(
         textFiles[language]![27],
         style: Theme.of(context).textTheme.labelLarge,
       ),
-      // Name des Buttons für die Registrierung aus der Sprachdatei
+    );
+  }
+
+  Widget _testButton() {
+    return BigButton(
+      onPressed: () {
+        Navigator.pushReplacementNamed(context, "/appHome");
+      },
+      buttonName: 'test',
     );
   }
 
@@ -96,34 +137,34 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          const ThemeContainer(), // Hintergrund-Themencontainer
+          const ThemeContainer(),
           Padding(
-            padding: const EdgeInsets.fromLTRB(
-                bottomPadding, sitesPadding, bottomPadding, sitesPadding), // Padding um den Inhalt
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 100,
-                ),
-                _logo(),
-                const SizedBox(
-                  height: 124,
-                ),
-                _emailTextField(),
-                const SizedBox(
-                  height: 8,
-                ),
-                _passwordTextField(),
-                const SizedBox(
-                  height: 32,
-                ),
-                _loginButton(),
-                _registerButton(),
-                const Expanded(child: SizedBox()),
-                _guestButton(),
-                const SizedBox(height: 32),
-              ],
+            padding: const EdgeInsets.fromLTRB(bottomPadding, sitesPadding, bottomPadding, sitesPadding),
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  _logo(),
+                  const SizedBox(
+                    height: 124,
+                  ),
+                  _emailTextField(),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  _passwordTextField(),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  _loginButton(),
+                  _registerButton(),
+                  const Expanded(child: SizedBox()),
+                  _testButton(),
+                ],
+              ),
             ),
           ),
         ],
