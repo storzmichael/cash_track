954530662eb628d9023fd1bf0549db436bf28728
storzmichael <micsto24@gmail.com>
2024-10-01 23:12:23 +0200

    added new function

--- lib/src/core/presentation/dialog_helper.dart
+++ lib/src/core/presentation/dialog_helper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:cash_track/src/config/config_colors.dart';
+import 'package:flutter/material.dart';
 
 class DialogHelper {
   // Reusable Cupertino dialog for text input
@@ -42,6 +43,52 @@ class DialogHelper {
               onPressed: () {
                 String inputText = textController.text.trim();
                 onConfirm(inputText); // Callback with the entered text
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Reusable Cupertino dialog without text input
+  static void showConfirmationDialog({
+    required BuildContext context,
+    required String title,
+    required String message,
+    required String cancelButtonText,
+    required String confirmButtonText,
+    required Function() onConfirm,
+  }) {
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: Text(title),
+          content: Column(
+            mainAxisSize: MainAxisSize.min, // Ensures the dialog is not oversized
+            children: [
+              const SizedBox(height: 16), // Adds spacing between title and content
+              Text(
+                message, // Message text
+                style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(height: 16), // Adds spacing below the message
+            ],
+          ),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              isDefaultAction: true,
+              child: Text(cancelButtonText, style: const TextStyle(color: blackColor)), // Cancel button style
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            CupertinoDialogAction(
+              child: Text(confirmButtonText, style: const TextStyle(color: primeryColor)), // Confirm button style
+              onPressed: () {
+                onConfirm(); // Callback for confirmation action
                 Navigator.of(context).pop();
               },
             ),
