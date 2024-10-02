81cb5d9d9584a5cac431a656eff01ee96812b280
storzmichael <micsto24@gmail.com>
2024-10-02 11:33:48 +0200

    added products

--- lib/src/features/order/data/category_data_map.dart
+++ lib/src/features/order/data/category_data_map.dart
@@ -1,3 +1,27 @@
 import 'package:cash_track/src/features/order/domain/product_item.dart';
 
-Map<String, List<ProductItem>> categoryData = {};
+Map<String, List<ProductItem>> categoryData = {
+  'Getränke': [
+    ProductItem(productTitle: 'Cola', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Fanta', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Med. Wasser', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Mineral Wasser', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'A-Schorle', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Bier', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Radler Süß', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Radler Sauer', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Weiswein', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Weiswein Schorle', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Rotwein', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Rotwein Schorle', productPrice: 2.50, productCategory: 'Getränke'),
+  ],
+  'Essen': [
+    ProductItem(productTitle: 'Pommes', productPrice: 2.50, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Salat', productPrice: 2.50, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Rote Wurst', productPrice: 2.50, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Curry Wurst', productPrice: 2.50, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Wurst Wecken', productPrice: 2.50, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Käse Wecken', productPrice: 2.50, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Toast', productPrice: 2.50, productCategory: 'Essen'),
+  ]
+};
