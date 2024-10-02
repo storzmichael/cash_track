134bcfa8c34f3ee2d7b508f975e136f28d597eec
storzmichael <micsto24@gmail.com>
2024-10-02 11:35:29 +0200

    added

--- lib/src/features/order/data/table_list.dart
+++ lib/src/features/order/data/table_list.dart
@@ -1,1 +1,13 @@
-List<String> tables = [];
+import 'package:cash_track/src/features/order/domain/product_item.dart';
+
+List<String> tables = ['1', '2', '3', '4', '5', '6', 'Theke'];
+List<Map<String, List<ProductItem>>> tables1 = [
+  {
+    '1': [],
+    '2': [],
+    '3': [],
+    '4': [],
+    '5': [],
+    '6': [],
+  }
+];
