b3276e41191e8d2e6eff0e5fc7eb7f8ed4437d20
storzmichael <micsto24@gmail.com>
2024-10-03 09:33:49 +0200

    added products

--- lib/src/features/order/data/category_data_map.dart
+++ lib/src/features/order/data/category_data_map.dart
@@ -1,3 +1,36 @@
 import 'package:cash_track/src/features/order/domain/product_item.dart';
 
-Map<String, List<ProductItem>> categoryData = {};
+Map<String, List<ProductItem>> categoryData = {
+  'Getränke': [
+    ProductItem(productTitle: 'Cola', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Fanta', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Med. Wasser', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Mineral Wasser', productPrice: 2.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Bier', productPrice: 3.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Radler Süß', productPrice: 3.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Radler Sauer', productPrice: 3.50, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Weizen', productPrice: 4.00, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Weiswein', productPrice: 3.00, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Weis Süß', productPrice: 3.00, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Weis Sauer', productPrice: 3.00, productCategory: 'Getränke'),
+    ProductItem(productTitle: 'Rotwein', productPrice: 3.50, productCategory: 'Getränke'),
+  ],
+  'Essen': [
+    ProductItem(productTitle: 'Pommes', productPrice: 3.50, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Grillwurst', productPrice: 3.00, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Currywurst', productPrice: 3.50, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Wurstwecken', productPrice: 2.50, productCategory: 'Essen'),
+    ProductItem(productTitle: 'Käsewecken', productPrice: 2.50, productCategory: 'Essen'),
+  ]
+};
+
+Map<String, List<ProductItem>> orderData = {};
+
+//test
+void addProductToOrder(ProductItem productItem) {
+  if (!orderData.containsKey(productItem.productCategory)) {
+    orderData[productItem.productCategory] = [];
+  }
+
+  orderData[productItem.productCategory]!.add(productItem);
+}
