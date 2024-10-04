bd1b475b84ca2d07987864a633b13b5117da5c8b
storzmichael <micsto24@gmail.com>
2024-10-04 16:38:49 +0200

    deleted import

--- lib/src/features/profil/presentation/profil_screen.dart
+++ lib/src/features/profil/presentation/profil_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cash_track/src/config/config.dart';
 import 'package:cash_track/src/data/lang/app_text.dart';
-import 'package:cash_track/src/core/presentation/theme_container.dart';
 import 'package:cash_track/src/features/general_widgets/presentation/outlined_big_button.dart';
 import 'package:cash_track/src/features/profil/presentation/circle_avatar_widget.dart';
 
