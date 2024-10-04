da28e30c7372805521c8ae467959f94f1133f23d
storzmichael <micsto24@gmail.com>
2024-10-04 16:37:17 +0200

    deleted list

--- lib/src/features/order/data/products_list.dart
+++ lib/src/features/order/data/products_list.dart
@@ -1,6 +1,3 @@
 import 'package:cash_track/src/features/order/domain/product_item.dart';
 
 List<ProductItem> productsData = [];
-
-// ausgewählt für Bestellung
-final List<ProductItem> selectedProducts = [];
