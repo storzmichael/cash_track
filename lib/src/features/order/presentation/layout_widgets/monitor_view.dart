5c127599a0a7c13c050706a23a3526427cd1c0ef
storzmichael <micsto24@gmail.com>
2024-10-10 15:30:15 +0200

    changed consumer on top of widget tree

--- lib/src/features/order/presentation/layout_widgets/monitor_view.dart
+++ lib/src/features/order/presentation/layout_widgets/monitor_view.dart
@@ -1,29 +1,25 @@
 import 'dart:developer';
-
 import 'package:cash_track/src/config/config.dart';
 import 'package:cash_track/src/config/config_colors.dart';
 import 'package:cash_track/src/data/lang/app_text.dart';
 import 'package:cash_track/src/features/cashout/presentation/cashout_screen.dart';
 import 'package:cash_track/src/features/order/application/order_provider.dart';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class MonitorView extends StatelessWidget {
   const MonitorView({super.key});
 
-  final double _monitorHeight = 160;
-  // Höhe des Monitors (Bildschirms) für die Anzeige
+  final double _monitorHeight = 160; // Höhe des Monitors (Bildschirms)
 
   @override
   Widget build(BuildContext context) {
-    final orderProvider = Provider.of<OrderProvider>(context);
-    return Column(
-      children: [
-        // Erste Zeile: Enthält Bestellungsdetails und den Tischmonitor
-        Consumer<OrderProvider>(
-          builder: (context, orderProvider, child) {
-            return Row(
+    return Consumer<OrderProvider>(
+      builder: (context, orderProvider, child) {
+        return Column(
+          children: [
+            // Erste Zeile: Enthält Bestellungsdetails und den Tischmonitor
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween, // Verteilung der Elemente in der Zeile
               children: [
                 // Anzeige der Bestellsumme (links)
@@ -39,7 +35,7 @@ class MonitorView extends StatelessWidget {
                         color: monitorColor, // Hintergrundfarbe
                       ),
                       child: ListView.builder(
-                        itemCount: orderProvider.selectedProducts.length, //, // Anzahl der bestellten Produkte
+                        itemCount: orderProvider.selectedProducts.length, // Anzahl der bestellten Produkte
                         itemBuilder: (context, index) {
                           final product = orderProvider.selectedProducts[index];
                           return SizedBox(
@@ -49,22 +45,10 @@ class MonitorView extends StatelessWidget {
                               title: Text(product.productTitle),
                               trailing: Text('${(product.productPrice * product.quantity).toStringAsFixed(2)}€'),
                               onTap: () {
-                                orderProvider.removeProduct(product, context);
+                                orderProvider.removeProductfromSelect(product, context);
                               },
                             ),
                           );
-
-                          /*Row(
-                          children: [
-                            const Text('1x'), // Beispiel für die Anzahl eines Produkts
-                            const SizedBox(
-                              width: 8, // Abstand zwischen Anzahl und Produktnamen
-                            ),
-                            Text(_orderItems[index].productTitle), // Anzeige des Produktnamens
-                            const Expanded(child: SizedBox()), // Füllt den verfügbaren Platz
-                            Text('${_orderItems[index].productPrice} €'), // Anzeige des Produktpreises
-                          ],
-                        );*/
                         },
                       ),
                     ),
@@ -83,80 +67,74 @@ class MonitorView extends StatelessWidget {
                         ),
                         color: monitorColor, // Hintergrundfarbe
                       ),
-                      child: Consumer<OrderProvider>(
-                        builder: (context, orderProvider, child) {
-                          return ListView.builder(
-                            itemCount: orderProvider.orderDeskNumbers.length,
-                            itemBuilder: (context, index) {
-                              final deskNumber = orderProvider.orderDeskNumbers[index];
+                      child: ListView.builder(
+                        itemCount: orderProvider.orderDeskNumbers.length,
+                        itemBuilder: (context, index) {
+                          final deskNumber = orderProvider.orderDeskNumbers[index];
 
-                              // Überprüfen, ob es für den Tisch Produkte gibt
-                              if (orderProvider.orderDeskProducts.containsKey(deskNumber) &&
-                                  orderProvider.orderDeskProducts[deskNumber]!.isNotEmpty) {
-                                return ListTile(
-                                  leading: Text('Tisch:'),
-                                  title: Text(deskNumber),
-                                  onTap: () {
-                                    // Setze die aktuelle Tisch-Nummer auf den ausgewählten Tisch
-                                    orderProvider.setDeskNumber(deskNumber);
+                          // Überprüfen, ob es für den Tisch Produkte gibt
+                          if (orderProvider.orderDeskProducts.containsKey(deskNumber) &&
+                              orderProvider.orderDeskProducts[deskNumber]!.isNotEmpty) {
+                            return ListTile(
+                              leading: Text('Tisch:'),
+                              title: Text(deskNumber),
+                              onTap: () {
+                                // Setze die aktuelle Tisch-Nummer auf den ausgewählten Tisch
+                                orderProvider.setDeskNumber(deskNumber);
 
-                                    // Logs zum Überprüfen der Produkte in der orderData
-                                    log('Produkte in orderData: ${orderProvider.orderDeskProducts.map((key, value) => MapEntry(key, value.toString()))}');
-                                    log('Aktuelle Auswahl $deskNumber: ${orderProvider.orderDeskProducts[deskNumber]}');
-                                    log('zustand des tisch: ${orderProvider.isTableSelect}');
-                                    log('Verfügbare tische: ${orderProvider.tables}');
+                                // Logs zum Überprüfen der Produkte in der orderData
+                                log('Produkte in orderData: ${orderProvider.orderDeskProducts.map((key, value) => MapEntry(key, value.toString()))}');
+                                log('Aktuelle Auswahl $deskNumber: ${orderProvider.orderDeskProducts[deskNumber]}');
+                                log('zustand des tisch: ${orderProvider.isTableSelect}');
+                                log('Verfügbare tische: ${orderProvider.tables}');
 
-                                    // Navigiere zum Cashout-Screen
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => CashoutScreen(selectedTable: deskNumber),
-                                      ),
-                                    );
-                                  },
+                                // Navigiere zum Cashout-Screen
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => CashoutScreen(selectedTable: deskNumber),
+                                  ),
                                 );
-                              } else {
-                                // Falls der Tisch keine Produkte enthält, wird kein ListTile angezeigt
-                                return const SizedBox.shrink(); // Rückgabe eines leeren Containers
-                              }
-                            },
-                          );
+                              },
+                            );
+                          } else {
+                            // Falls der Tisch keine Produkte enthält, wird kein ListTile angezeigt
+                            return const SizedBox.shrink(); // Rückgabe eines leeren Containers
+                          }
                         },
                       ),
                     ),
                   ),
                 ),
               ],
-            );
-          },
-        ),
-        const SizedBox(
-          height: 8, // Abstand zwischen den Elementen
-        ),
-        // Zweite Zeile: Anzeige der Gesamtsumme
-        SizedBox(
-          height: 40, // Höhe des Summenfelds
-          width: double.infinity, // Nimmt die gesamte Breite ein
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(8), // Abgerundete Ecken des Containers
-              ),
-              color: textFieldColor, // Hintergrundfarbe
             ),
-            child: Padding(
-              padding: const EdgeInsetsDirectional.symmetric(horizontal: 8.0), // Innenabstand links und rechts
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween, // Verteilung von Texten am Rand
-                children: [
-                  Text(textFiles[language]![4]), // Text für "Gesamtsumme" oder ähnliches
-                  Text('${orderProvider.totalPrice.toStringAsFixed(2)}€'), // Anzeige der Gesamtsumme
-                ],
+            const SizedBox(height: 8), // Abstand zwischen den Elementen
+            // Zweite Zeile: Anzeige der Gesamtsumme
+            SizedBox(
+              height: 40, // Höhe des Summenfelds
+              width: double.infinity, // Nimmt die gesamte Breite ein
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8), // Abgerundete Ecken des Containers
+                  ),
+                  color: textFieldColor, // Hintergrundfarbe
+                ),
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.symmetric(horizontal: 8.0), // Innenabstand links und rechts
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween, // Verteilung von Texten am Rand
+                    children: [
+                      Text(textFiles[language]![4]), // Text für "Gesamtsumme" oder ähnliches
+                      Text('${orderProvider.totalPrice.toStringAsFixed(2)}€'), // Anzeige der Gesamtsumme
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ),
-        )
-      ],
+            )
+          ],
+        );
+      },
     );
   }
 }
