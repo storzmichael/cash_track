b4460f6c73c5a5c353b3374deb8ff2ba54f46a2d
storzmichael <micsto24@gmail.com>
2024-11-14 08:41:58 +0100

    translation

--- lib/src/data/lang/english.dart
+++ lib/src/data/lang/english.dart
@@ -1,88 +1,93 @@
 List<String> english = [
-  'Event', // 0
-  'my Events', // 1
-  'Profile', // 2
-  'Table Number', // 3
+  'event', // 0
+  'my events', // 1
+  'profile', // 2
+  'table number', //3
   'Total', // 4
-  'Complete Payment', // 5
+  'Complete payment', // 5
   'Pending', // 6
-  'Please create a category.', // 7
+  'Please create a category', // 7
   'No items in this category.', // 8
   'Please enter something', // 9
-  'Table named', // 10
-  'already exists!', // 11
-  'Add New Table', // 12
-  'Enter Table Name', // 13
+  'Table with the name', // 10
+  'already exists', // 11
+  'Add new table', // 12
+  'Enter table name', // 13
   'Cancel', // 14
   'Add', // 15
-  'Delete Button', // 16
+  'Delete button', // 16
   'Do you really want to delete this button?', // 17
   'Delete', // 18
-  'Edit Profile', // 19
+  'Change profile', // 19
   'Login', // 20
   'Simply Smart Checkout', // 21
   'E-mail', // 22
   'Password', // 23
   'Password incorrect', // 24
   'Stay logged in', // 25
-  'Forgot password?', // 26
-  'Create New Account', // 27
+  'Forgot your password?', // 26
+  'Register', // 27
   'Please enter', // 28
-  'in', // 29
+  'enter', // 29
   'Please enter a valid email', // 30
   'Username must be at least 6 characters long', // 31
   'Password must be at least 6 characters long', // 32
   'Username', // 33
-  'Invalid inputs', // 34
-  'Complete Registration', // 35
-  'Create Products', // 36
-  'Product Name', // 37
-  'Product Price', // 38
-  'Product Category', // 39
-  'Product List by Categories', // 40
-  'to the Event', // 41
-  'Create New Event', // 42
-  'Generate New Code', // 43
-  'Create New Event', // 44
+  'Input invalid', // 34
+  'Complete registration', // 35
+  'Create products', // 36
+  'Product name', // 37
+  'Product price', // 38
+  'Product category', // 39
+  'Product list by category', // 40
+  'To event', // 41
+  'Create new event', // 42
+  'Generate new code', // 43
+  'Create new event', // 44
   'Order', // 45
-  'Select Table Number', // 46
-  'Join as Guest', // 47
+  'Select table number', // 46
+  'Join as guest', // 47
   'Registration', // 48
   'Settings', // 49
-  'Please enter your email and password', // 50
+  'Please enter e-mail and password', // 50
+
   'An account with this email address already exists.', // 51
   'An unknown error has occurred.', // 52
   'Error occurred', // 53
   'Error', // 54
   'OK', // 55
   'Logout failed', // 56
-  'Data Protection', // 57
+  'Data protection', // 57
   'Language', // 58
-  'Connected Devices', // 59
+  'Connected devices', // 59
   'Help', // 60
   'Logout', // 61
   'German', // 62
   'English', // 63
-  'New Event', // 64
+  'New event', // 64
   'Join Event', // 65
-  'Event Name', // 66
+  'Event name', // 66
   'Creator', // 67
   'Code', // 68
-  'Please enter your email and password', // 69
+  'Please enter email and password', // 69
   'An unexpected error has occurred', // 70
-  'Please enter a valid price.', // 71
-  'Really delete product?', // 72
-  'Please enter a valid price.', // 73
+  'Please enter a valid price', // 71
+  'Are you sure you want to delete the product?', // 72,
+  'Please enter a valid price', // 73
   'Product:', // 74
-  'One of the fields is empty, please fill out all fields.', // 75
+  'One of the fields is empty, please fill in all fields.', // 75
   'There are already 12 products in this category. No more products can be added.', // 76
   'This product is no longer available.', // 77
-  'This product is not in the cart.', // 78
-  'Product not found', // 79
-  'What would you like to do?', // 80
-  'to the product', // 81
-  'Leave Event', // 82
+  'This product is not in the shopping cart.', // 78
+  'Product not found', // 79q
+  'What do you want to do?', // 80
+  'To the product', // 81
+  'Leave event', // 82
   'Table:', // 83
-  'Forgot your password?', // 84
-  'Please provide email address', // 85
+  'Forgot password?', // 84
+  'Please submit email address', // 85
+  'We will send you an email to reset your password', // 86
+  'Send', // 87
+  'All products sold', // 88
+  'Evaluation events', // 89
 ];
